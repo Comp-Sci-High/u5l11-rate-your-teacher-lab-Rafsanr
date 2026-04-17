1fd1681bc8519ae178379185bd82dfdb7a827f79
Rafsanr <rafsan.ratul26@compscihigh.org>
2026-04-17 19:10:54 +0000

    part 2 done

--- public/ratingScript.js
+++ public/ratingScript.js
@@ -1,2 +1,19 @@
 // Add an event listener to the form that upon submits creates a new rating
 // When the rating is created redirect to the ratings page
+const addForm = document.querySelector("form")
+
+addForm.addEventListener("submit", async(event) => {
+    event.preventDefault()
+
+    const ratingSchema = new FormData(addForm)
+    const reqBody = Object.fromEntries(ratingSchema)
+
+    const response = await fetch("/add/rating", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(reqBody),
+    });
+    window.location.href = "/ratings"
+})
--- public/teacherScript.js
+++ public/teacherScript.js
@@ -1,3 +1,19 @@
 // Add an event listener to the form that upon submits creates a new teacher
 // When the teacher is created redirect to the teachers page
-const createForm = document.querySelector("")
+const createForm = document.querySelector("form")
+
+createForm.addEventListener("submit", async(event) => {
+    event.preventDefault()
+
+    const teacherSchema = new FormData(createForm)
+    const reqBody = Object.fromEntries(teacherSchema)
+
+    const response = await fetch("/add/teacher", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(reqBody),
+    });
+    window.location.href = "/"
+})
